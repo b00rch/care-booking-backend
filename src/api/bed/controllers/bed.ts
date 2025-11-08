ba7fbfccdcb6cd5bba9cb42b7ecaaebd46097c4a
Bold <m.boorch@gmail.com>
2025-11-08 13:51:32 +0800

    feat: add Strapi room and booking APIs

---
+++ src/api/bed/controllers/bed.ts
@@ -0,0 +1,7 @@
+/**
+ * bed controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::bed.bed');
---
+++ src/api/bed/services/bed.ts
@@ -0,0 +1,7 @@
+/**
+ * bed service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::bed.bed');
