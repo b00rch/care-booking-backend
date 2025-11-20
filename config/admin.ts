970803eb6170db777d91434ee3904941bebf90af
Bold <m.boorch@gmail.com>
2025-11-20 16:17:15 +0800

    ci: migrate backend image build to gitlab

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,11 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      enabled: true,
+      maxSessionLifespan: env('ADMIN_SESSION_MAX_LIFESPAN', '1d'),
+      maxRefreshTokenLifespan: env('ADMIN_SESSION_MAX_REFRESH_LIFESPAN', '7d'),
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
